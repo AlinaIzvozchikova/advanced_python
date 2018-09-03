18a006e73820bfaaf524241ac80efa2173da4ef9
alishka <alina_iz@mail.ru>
2018-09-03 23:58:52 +0300

    Homework2 - python crash

---
+++ run_homework2.py
@@ -0,0 +1,38 @@
+"""
+    Homework for Advanced Python course.
+    Task: Write scipt which causes segmentation fault(python3).
+"""
+
+import ctypes
+import sys
+
+
+def crash_using_ctypes(start_bool_value):
+    """
+    Crash python using ctypes lib.
+
+    :param start_bool_value: bool start value
+    :return:
+    """
+    bool_var = ctypes.c_bool(start_bool_value)
+    bool_pointer = ctypes.pointer(bool_var)
+
+    for i in range(10000):
+        bool_pointer[i] = bool_var
+
+
+def crash_using_recursion_limit(recursion_limit):
+    """
+    Resource exhaustion crash the python interpreter.
+
+    :param recursion_limit: integer value for recursion limit
+    :return:
+    """
+    sys.setrecursionlimit(10000)
+    rec_function = lambda f: rec_function(f)
+    rec_function(rec_function)
+
+
+if __name__ == '__main__':
+    crash_using_recursion_limit(10000)
+    # crash_using_ctypes(True)
